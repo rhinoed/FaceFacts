56433866e805337b4ea1ad85354af4bb70ca9609
Mark Edmunds <edmundsmd1@gmail.com>
2024-03-28 18:23:46 -0700

    Initial Commit

---
+++ FaceFacts/FaceFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceFactsApp.swift
+//  FaceFacts
+//
+//  Created by Mark Edmunds on 3/28/24.
+//
+
+import SwiftUI
+
+@main
+struct FaceFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
