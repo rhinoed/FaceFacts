add2de0796997c8b1f8e1a2432f2d8b1a066092a
Mark Edmunds <36555602+rhinoed@users.noreply.github.com>
2024-10-22 13:29:23 -0700

    delete Event rename Event 2

---
+++ Event.swift
@@ -0,0 +1,12 @@
+// Event.swift
+
+// Mark Edmunds
+
+//10-18-24
+
+@Model
+class Event{
+    var title: String
+    var location: String
+    var date: Date?
+}
--- FaceFacts/Event.swift
+++ FaceFacts/Event.swift
@@ -3,10 +3,18 @@
 // Mark Edmunds
 
 //10-18-24
+import Foundation
+import SwiftData
 
 @Model
 class Event{
     var title: String
     var location: String
     var date: Date?
+    
+    init(title: String, location: String, date: Date? = nil) {
+        self.title = title
+        self.location = location
+        self.date = date
+    }
 }
